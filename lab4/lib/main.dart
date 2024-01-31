d413eb505bc15ef249419dc1f0d6e993ab0cf371
Kritta <142368906+Krittanai2933@users.noreply.github.com>
2024-01-31 11:27:25 +0700

    Update main.dart

--- lab4/lib/main.dart
+++ lab4/lib/main.dart
@@ -23,47 +23,50 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Product Listing")),
+      appBar: AppBar(title: Text("6488053")),
       body: ListView(
         shrinkWrap: true,
         padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
         children: [
           ProductBox(
-            name: "iPhone",
-            description: "iPhone is the stylist phone ever",
-            price: 1000,
-            image: "Iphone.png",
-          ),
+              name: "iPhone",
+              description: "iPhone is the stylist phone ever",
+              price: 1000,
+              image:
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcWm6LQGK7BzRAi8Sj9X9vDJjGd7vZZO27Ug&usqp=CAU",
+              isNetworkImage: true),
           ProductBox(
-            name: "Pixel",
-            description: "Pixel is the most featureful phone ever",
-            price: 800,
-            image: "Pixel.png",
-          ),
+              name: "Pixel",
+              description: "Pixel is the most featureful phone ever",
+              price: 800,
+              image: "Pixel.png",
+              isNetworkImage: false),
           ProductBox(
-            name: "Laptop",
-            description: "Laptop is most productive development tool",
-            price: 2000,
-            image: "Laptop.png",
-          ),
+              name: "Laptop",
+              description: "Laptop is most productive development tool",
+              price: 2000,
+              image:
+                  "https://m.media-amazon.com/images/I/61hTLkamgBL._AC_UF894,1000_QL80_.jpg",
+              isNetworkImage: true),
           ProductBox(
-            name: "Tablet",
-            description: "Tablet is the most useful device ever for meeting",
-            price: 1500,
-            image: "Tablet.png",
-          ),
+              name: "Tablet",
+              description: "Tablet is the most useful device ever for meeting",
+              price: 1500,
+              image: "Tablet.png",
+              isNetworkImage: false),
           ProductBox(
-            name: "Pendrive",
-            description: "Pendrive is a useful storage medium",
-            price: 100,
-            image: "Pendrive.png",
-          ),
+              name: "Pendrive",
+              description: "Pendrive is a useful storage medium",
+              price: 100,
+              image: "Pendrive.png",
+              isNetworkImage: false),
           ProductBox(
-            name: "Floppy Drive",
-            description: "Floppy drive is a useful rescue storage medium",
-            price: 20,
-            image: "Floppy.png",
-          ),
+              name: "Floppy Drive",
+              description: "Floppy drive is a useful rescue storage medium",
+              price: 20,
+              image:
+                  "https://th.bing.com/th/id/R.52f7219984fdf56dc288fbab81840a7f?rik=vgi8IFRNzdayiQ&pid=ImgRaw&r=0",
+              isNetworkImage: true),
         ],
       ),
     );
@@ -77,13 +80,14 @@ class ProductBox extends StatelessWidget {
     required this.description,
     required this.price,
     required this.image,
+    required this.isNetworkImage,
   }) : super(key: key);
 
   final String name;
   final String description;
   final int price;
   final String image;
-
+  final bool isNetworkImage;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -93,7 +97,9 @@ class ProductBox extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Image.asset("appimages/" + image),
+            isNetworkImage
+                ? Image.network(image, width: 80, height: 80)
+                : Image.asset("appimages/" + image, width: 80, height: 80),
             Expanded(
               child: Container(
                 padding: EdgeInsets.all(5),
